83721c6c1997e3f0ad99e2491f0350020079ed63
RMA <RMA@bab10101-e421-0410-a517-8ce0973de3ef>
2011-04-15 12:50:00 +0000

    SOLR-11 : Remove plugin-document dependency
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/module-document-solr@28376 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/document/modules/solr/service/DocumentSolrPlugin.java
@@ -0,0 +1,9 @@
+package fr.paris.lutece.plugins.document.modules.solr.service;
+
+import fr.paris.lutece.portal.service.plugin.PluginDefaultImplementation;
+
+public class DocumentSolrPlugin extends PluginDefaultImplementation
+{
+	public static final String PLUGIN_NAME = "document-solr";
+	
+}
